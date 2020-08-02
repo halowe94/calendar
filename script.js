4a3fbdd5f70f10f635f72ffa8392321011b68f70
H Lowe <haleylowe0@gmail.com>
2020-08-02 14:51:24 -0700

    added ore of the outline to html

---
+++ script.js
@@ -0,0 +1,9 @@
+//create outline of calendar in bootstrap
+//each row in html is an input type
+//when user clicks a time block, 
+    //the user is able to type
+//addeventListener to save buttons
+//function for save buttons 
+//when user clicks save button
+    //the input stays in the box
+    //the background color of the row changes
