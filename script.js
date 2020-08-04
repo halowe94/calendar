737d2c0c878f1380daccadb3c763e2b3700da0f1
H Lowe <haleylowe0@gmail.com>
2020-08-03 23:00:23 -0700

    added function to save input to local storage. added ids to each input

--- script.js
+++ script.js
@@ -8,5 +8,39 @@
     //the input stays in the box
     //the background color of the row changes
 
-let content = document.getElementsByClassName("inputArea")
-    content.contentEditable = "true";
+
+
+    $(".saveBtn").click( function() {
+        let inputText = $(this).parent().siblings(".inputTag").val();
+        let inputTime = $(this).parent().siblings(".inputTag").attr("id")
+        console.log(inputTime);
+        localStorage.setItem(inputTime, JSON.stringify(inputText));
+
+    })
+
+    //   //this is whatever is clicked
+    // //localStorage.getItem
+    // function retrieveItems(){
+    //     //$(#9am).val() or =
+    //     let get9 = $("#9am");
+    //     let get10 = $("#10am");
+    //     let get11 = $("#11am");
+    //     let get12 = $("#12pm");
+    //     let get1 = $("#1pm");
+    //     let get2 = $("#2pm");
+    //     let get3 = $("#3pm");
+    //     let get4 = $("#4pm");
+    //     let get5 = $("#5pm");
+    
+    //     localStorage.getItem(get9).val();
+    //     localStorage.getItem(get10).val();
+    //     localStorage.getItem(get11).val();
+    //     localStorage.getItem(get12).val();
+    //     localStorage.getItem(get1).val();
+    //     localStorage.getItem(get2).val();
+    //     localStorage.getItem(get3).val();
+    //     localStorage.getItem(get4).val();
+    //     localStorage.getItem(get5).val();
+    //     }
+    
+    //     retrieveItems().style.display = "block"; 
