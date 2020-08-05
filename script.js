3d8ab3cf053d3d4279ceb86501a9f8570ffedddb
H Lowe <haleylowe0@gmail.com>
2020-08-04 21:46:41 -0700

    adding to js to use localStorage in html

--- script.js
+++ script.js
@@ -13,26 +13,34 @@
     $(".saveBtn").click( function() {
         let inputText = $(this).parent().siblings(".inputTag").val();
         let inputTime = $(this).parent().siblings(".inputTag").attr("id")
-        console.log(inputTime);
+        console.log(inputText);
         localStorage.setItem(inputTime, JSON.stringify(inputText));
 
     })
 
     //   //this is whatever is clicked
     // //localStorage.getItem
-    // function retrieveItems(){
-    //     //$(#9am).val() or =
-/    // let get9 = $("#9am");
-    //     let get10 = $("#10am");
-    //     let get11 = $("#11am");
-    //     let get12 = $("#12pm");
-    //     let get1 = $("#1pm");
-    //     let get2 = $("#2pm");
-    //     let get3 = $("#3pm");
-    //     let get4 = $("#4pm");
-    //     let get5 = $("#5pm");
-    
-         localStorage.getItem($("#9am")).val(saveBtn);
+    function retrieveItems(){
+        getNine();
+    }
+         //localStorage.getItem($("#9am").val());
+    window.localStorage.getItem('#9am');
+          $('#9am').text= function() {
+              localStorage.key("9am").val()
+          }
+
+        
+    let getTen = window.localStorage.getItem('#10am');
+    let getEleven = window.localStorage.getItem('#11am');
+    let getTwelve = window.localStorage.getItem('#12pm');
+    let getOne = window.localStorage.getItem('#1pm');
+    let getTwo = window.localStorage.getItem('#2pm');
+    let getThree = window.localStorage.getItem('#3pm');
+    let getFour = window.localStorage.getItem('#4pm');
+    let getFive = window.localStorage.getItem('#5pm');
+
+
+
     //     localStorage.getItem(get10).val();
     //     localStorage.getItem(get11).val();
     //     localStorage.getItem(get12).val();
